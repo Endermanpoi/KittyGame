d16d8492ac61c05f055878e74af8cf1085c1f49c
endermanpoi <lqx2112@foxmail.com>
2018-06-20 19:27:33 +0800

    done

--- public/javascripts/initWeb3.js
+++ public/javascripts/initWeb3.js
@@ -3,13 +3,24 @@ var web3;
 window.addEventListener('load', function () {
 	if (typeof web3 !== 'undefined') {
 		web3 = new Web3(web3.currentProvider);
+		console.log("MetaMask!");
 	} else {
-		console.log("noMist!");
+		console.log("noMetaMask!");
+		document.getElementById('main').innerHTML =
+			"<div class=\"Hero\">" +
+			"<div class=\"Container Container--sm Container--center\">" +
+			"<h2 class=\"Hero-h2\">喵呜，似乎您没有打开并登陆您的MetaMask</h2>" +
+			"<p class=\"Hero-description\">只需打开MetaMask登陆并连接到我们的<strong>私有链网络</strong>即可。</p>" +
+			"<img class=\"Hero-image\" src=\"/images/main-network.png\" alt=\"主要以太坊网络\">" +
+			"</div>" +
+			"</div>" +
+			"</main>";
 	}
 	web3.version.getNetwork(function (err, netId) {
 		if (netId == 519)
-			console.log('This is the private network.');
-		else
+			console.log("This is the private network.");
+		else {
+			console.log("This is not private network.");
 			document.getElementById('main').innerHTML =
 				"<div class=\"Hero\">" +
 				"<div class=\"Container Container--sm Container--center\">" +
@@ -19,7 +30,8 @@ window.addEventListener('load', function () {
 				"</div>" +
 				"</div>" +
 				"</main>";
-	})
+		}
+	});
 	setInterval(function () {
 		if (web3.eth.accounts[0] !== userAccount) {
 			userAccount = web3.eth.accounts[0];
