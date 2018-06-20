a13bdac58d0cd5b89f6819ec8432f33bf2b4031a
endermanpoi <lqx2112@foxmail.com>
2018-06-20 23:27:25 +0800

    cat

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'KittyGame' });
+router.get('/', function (req, res, next) {
+  var filesList = fs.readdirSync('./public/images/cat/');
+  var catID = filesList.length;
+  res.render('index', { title: 'KittyGame', cat: catID });
 });
 
 module.exports = router;
