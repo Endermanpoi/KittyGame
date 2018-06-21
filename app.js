ab548cd384914c6324b639166fad2f005eafdea4
endermanpoi <lqx2112@foxmail.com>
2018-06-21 18:05:31 +0800

    error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./controller/routes');
+var indexRouter = require('./controllers/routes');
 
 var app = express();
 
