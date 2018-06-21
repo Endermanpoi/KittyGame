49be109fa4f886321ca5209ea238d75c6b95c554
endermanpoi <lqx2112@foxmail.com>
2018-06-21 23:48:10 +0800

    loading

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routesRouter);
-app.use('/run', executeRouter);
+app.use('/', executeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
